a71763e8f7f35b915e971f4070fab31935d3cc5a
aix27249 <i27249@gmail.com>
2014-04-22 01:42:21 +0400

    pkgsearch: ability to search by different fields

--- ui/modules/pkgsearch/module.php
+++ ui/modules/pkgsearch/module.php
@@ -6,8 +6,31 @@ Page::loadModule('pkglist');
 class Module_pkgsearch extends RepositoryModule {
 	static $requires = ['repository', 'pkglist'];
 	public function run() {
-		$regex = new MongoRegex('/^.*' . $_GET['q'] . '.*$/i');
-		$q = $this->db->packages->find(['name' => $regex]);
-		return Module_pkglist::getList($q/*, (@$_GET['limit'] ? intval($_GET['limit']) : 50), @$_GET['offset']*/);
+		$query = [];
+		if (isset($_GET['name'])) $query['name'] = trim($_GET['name']);
+		else if (isset($_GET['q'])) {
+			$get_q = preg_replace('/\+/', '\+', trim($_GET['q']));
+			$query['name'] = new MongoRegex('/' . $get_q . '/i');
+		}
+		if (isset($_GET['version'])) {
+			$query['version'] = trim($_GET['version']);
+		}
+		if (isset($_GET['arch'])) {
+			$query['arch'] = trim($_GET['arch']);
+		}
+		if (isset($_GET['build'])) {
+			$query['build'] = trim($_GET['build']);
+		}
+		if (isset($_GET['by'])) {
+			$query['maintainer.name'] = trim($_GET['by']);
+		}
+		if (isset($_GET['latest'])) {
+			$query['latest'] = 1;
+		}
+
+
+		$q = $this->db->packages->find($query);
+		$ret = '<h1>Search results</h1>';
+		return $ret . Module_pkglist::getList($q/*, (@$_GET['limit'] ? intval($_GET['limit']) : 50), @$_GET['offset']*/);
 	}
 }
