af9e783265e229f66fe4527ecd449c8d93b95309
aix27249 <i27249@gmail.com>
2014-04-24 01:44:38 +0400

    UI template: add popup support

--- ui/templates/repository/template.php
+++ ui/templates/repository/template.php
@@ -10,7 +10,7 @@
 class Template extends GenericTemplate {
 
 	// Template styles (optional, but who wants to use template without CSS?)
-	public static $styles = ['defaults.css', 'layout.css', 'header.css', 'content.css', 'footer.css', 'fonts.css'];
+	public static $styles = ['defaults.css', 'layout.css', 'header.css', 'content.css', 'footer.css', 'fonts.css', 'popup.css'];
 	
 	// Template scripts (optional)
 	public static $scripts = ['jquery-1.9.1.min.js', 'jquery.color-2.1.2.min.js', 'jquery.scrollTo.min.js'];
