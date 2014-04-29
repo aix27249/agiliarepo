1a5634a4950e599ae04c68a6cbd0d7de2bb4256d
aix27249 <i27249@gmail.com>
2014-04-30 03:48:38 +0400

    MongoDBObject class: abstraction layer to access and modify MongoDB objects

--- core/mongo.class.php
+++ core/mongo.class.php
@@ -12,13 +12,13 @@ class MongoConnection {
 				if (!isset($mongo_config['options'])) $mongo_config['options'] = ['connect' => true];
 				if (!isset($mongo_config['driver_options'])) $mongo_config['driver_options'] = [];
 				static::$connection = new MongoClient($mongo_config['server'], $mongo_config['options'], $mongo_config['driver_options']);
-				
+
 			}
 			else {
 				static::$connection = new MongoClient();
 			}
 		}
-		
+
 		return static::$connection;
 	}
 }
@@ -36,3 +36,65 @@ class MongoDBAdapter {
 
 	}
 }
+
+abstract class MongoDBObject extends MongoDBAdapter {
+	protected $data = [];
+	protected $old_data = NULL;
+	protected $id_key = NULL;
+
+	public function __get($key) {
+		if (isset($this->data[$key])) return $this->data[$key];
+		return NULL;
+	}
+
+	public function __set($key, $value) {
+		if ($this->old_data===NULL) $this->old_data = $this->data;
+
+		$this->data[$key] = $value;
+	}
+	public function storeState($force = true) {
+
+		if ($force || $this->old_data===NULL) $this->old_data = $this->data;
+	}
+
+	protected function load($collection, $query) {
+		$this->data = self::db()->$collection->findOne($query);
+	}
+
+	abstract public function __construct($id = NULL);
+
+	public function save($dry_run = false) {
+		if ($this->id_key===NULL) throw new Exception('Save failed: class' . get_class() . ' has no id_key defined');
+
+		$changeset = [];
+		foreach($this->data as $key => $value) {
+			if (@$this->old_data[$key]!=$value) $changeset[$key] = $value;
+		}
+
+		if ($dry_run) {
+			print_r($changeset);
+			return;
+		}
+
+		if (count($changeset)===0) return;
+		
+		$searchquery = 	[
+			$this->id_key => $this->data[$this->id_key], 
+			'_rev' => $this->data['_rev']
+			]; 
+
+		$setquery = [
+			'$set' => $changeset, 
+			'$inc' => ['_rev' => 1]
+			];
+
+
+		self::db()->packages->findAndModify($searchquery, $setquery);
+
+
+
+
+	}
+}
+
+
