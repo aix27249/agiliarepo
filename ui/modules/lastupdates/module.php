d377367d648b1a8ce1c2b9cc26599be3faf864cc
aix27249 <i27249@gmail.com>
2014-05-03 23:02:29 +0400

    lastupdates: display latest updates only from default repository;

--- ui/modules/lastupdates/module.php
+++ ui/modules/lastupdates/module.php
@@ -4,8 +4,14 @@ Page::loadModule('pkglist');
 
 class Module_lastupdates extends RepositoryModule {
 	public function run() {
-		$ret = '<h1>Last ' . (@$_GET['limit'] ? intval($_GET['limit']) : 50) . ' updates</h1>';
-		$ret .= Module_pkglist::getList($this->db->packages->find()->sort(['add_date' => -1]), (@$_GET['limit'] ? intval($_GET['limit']) : 50), @$_GET['offset']);
+		$default_repository = Settings::get('default_repository');
+		$query = [];
+		if ($default_repository) $query['repositories.repository'] = $default_repository;
+		
+		$ret = '<h1>Latest updates in ' . $default_repository . '</h1>';
+
+		$packages = self::db()->packages->find($query)->sort(['add_date' => -1]);
+		$ret .= Module_pkglist::getList($packages, 20, 0, 'Complex', false);
 		return $ret;
 	}
 }
