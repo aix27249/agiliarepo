9a61890aa890f960f059eac0e8f08bc1011c8d41
aix27249 <i27249@gmail.com>
2014-05-03 23:01:16 +0400

    add static wrappers for settings

--- core/settings.class.php
+++ core/settings.class.php
@@ -6,6 +6,7 @@ require_once 'mongo.class.php';
 class Settings extends MongoDBAdapter {
 	private $realtime = true;
 	private $fields = [];
+	private static $instance = NULL;
 
 	public function __construct($realtime = true) {
 		$this->realtime = $realtime;
@@ -13,12 +14,26 @@ class Settings extends MongoDBAdapter {
 	}
 
 	public function __get($key) {
-		if ($this->realtime) return self::db()->settings->findOne(['key' => $key]);
+		if ($this->realtime) {
+			$obj = self::db()->settings->findOne(['key' => $key]);
+			if (isset($obj['value'])) return $obj['value'];
+		}
 		else return @$this->fields[$key];
 	}
+
+	public static function get($key) {
+		if (!self::$instance) self::$instance = new self(true);
+		return self::$instance->$key;
+	}
+
 	public function __set($key, $value) {
 		if ($this->realtime) self::db()->settings->update(['key' => $key], ['key' => $key, 'value' => $value], ['upsert' => 1]);
 		else $this->fields[$key] = $value;
+	}
+
+	public static function set($key, $value) {
+		if (!self::$instance) self::$instance = new self(true);
+		self::$instance->$key = $value;
 	}
 
 	public function clear($key) {
