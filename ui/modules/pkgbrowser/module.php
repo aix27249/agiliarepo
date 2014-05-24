a099ba85fdf4259d580e4c37a3dd8d02a4ba3148
aix27249 <i27249@gmail.com>
2014-05-25 01:54:40 +0400

    fix package query in pkgbrowser

--- ui/modules/pkgbrowser/module.php
+++ ui/modules/pkgbrowser/module.php
@@ -8,10 +8,14 @@ class Module_pkgbrowser extends RepositoryModule {
 		if ($this->blockname==='sidebar') return $this->run_sidebar();
 		$path = array_slice($this->page->path, 2);
 		$query = [];
-		if (isset($path[0])) $query['repositories.repository'] = $path[0];
-		if (isset($path[1])) $query['repositories.osversion'] = $path[1];
-		if (isset($path[2])) $query['repositories.branch'] = $path[2];
-		if (isset($path[3])) $query['repositories.subgroup'] = $path[3];
+		$ematch = [];
+		if (isset($path[0])) $ematch['repository'] = $path[0];
+		if (isset($path[1])) $ematch['osversion'] = $path[1];
+		if (isset($path[2])) $ematch['branch'] = $path[2];
+		if (isset($path[3])) $ematch['subgroup'] = $path[3];
+		if (count($ematch)>0) {
+			$query['repositories'] = ['$elemMatch' => $ematch];
+		}
 
 		$newpath = array_merge(['/'], $path);
 		$page = intval(@$_GET['page']);
